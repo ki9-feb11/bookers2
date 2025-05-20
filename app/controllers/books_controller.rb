4fe401adaded7d098d4025bedffbfa60af22f54d
ki9-feb11 <kara.kiku0211@gmial.com>
2025-05-20 20:34:17 +0000

    [Add]viewファイル

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,25 @@
+class BooksController < ApplicationController
+
+  def new
+    @book = Books.new
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def upadate
+  end
+
+  def destroy
+  end
+
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def upadate
+  end
+
+end

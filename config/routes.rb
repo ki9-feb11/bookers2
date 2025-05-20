e2586b3f18ab89901470211da675bdc215555a89
ki9-feb11 <kara.kiku0211@gmial.com>
2025-05-20 20:54:43 +0000

    [Add]bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'homes/top'
+  get 'homes/about'
   devise_for :users
   root to: "homes#top"
   resources :books
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :index, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

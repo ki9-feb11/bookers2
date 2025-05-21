bed089c8fd03f02c8d4150f3e08b07c0f86ab411
ki9-feb11 <kara.kiku0211@gmial.com>
2025-05-21 06:25:32 +0000

    [Add]nameでログイン

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         authentication_keys: [:username]
+         authentication_keys: [:name]
 
   has_many :books, dependent: :destroy
+
+  def email_required?
+    true
+  end
+
+  def will_save_change_to_email?
+    false
+  end
 
 end
